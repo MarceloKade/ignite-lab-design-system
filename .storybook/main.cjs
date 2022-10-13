e6f7345d5b83f5dea141f2a8d7c27ef20fcf9442
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2022-10-13 01:46:56 -0300

    ci: Add publish storybook workflow correct

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "ignite-lab-design-system/";
+      config.base = "/ignite-lab-design-system/";
     }
     return config;
   },
